ce9e6a51be7c3750f9d69e6a06f875d1af02869a
Varchas Jasti <jastivarchas@gmail.com>
2026-06-28 09:53:12 +0530

    add comments for task 3

--- task-03-deployment/backend/index.js
+++ task-03-deployment/backend/index.js
@@ -1,11 +1,20 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
+/*
+Alright so I'm basically creating a file for the backend server.
+I want to start the server on port 3000 and expose one API end point.
+*/
 
+// Import the Express Library into the project
+const express = require('express');
+// I'm creating an Express Application Instance
+const app = express();
+const PORT = 3000;  // This is just a contant to store the port number
+
+// I'm creating a GET API endpoint at /api/name
 app.get('/api/name', (req, res) => {
-  res.json({ name: 'Varchas Jasti' });
+  res.json({ name: 'Varchas Jasti' });  // This is the required response we want to give
 });
 
+// Imma start the Express Server after giving the response, so it can listen for incoming requests
 app.listen(PORT, () => {
   console.log(`Backend running on port ${PORT}`);
 });
